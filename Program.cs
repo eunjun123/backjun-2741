6a78868e3cca35c32b1e531c7789416c93c68d22
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 18:56:09 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int temp = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= temp; i++)
+            {
+                stringBuilder.Append(i + "\n");
+            }
+            Console.WriteLine(stringBuilder);
+        }
+    }
